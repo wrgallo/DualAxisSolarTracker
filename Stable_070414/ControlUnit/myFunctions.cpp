a804e24f22e9d7178e215e7eb8d2e09421b5bafa
wrgallo <wrgallo@hotmail.com>
2018-07-04 14:08:29 -0300

    Add files via upload

---
+++ Stable_070414/ControlUnit/myFunctions.cpp
@@ -0,0 +1,245 @@
+#include "Arduino.h"
+#include "myFunctions.h"
+//#include "myEEPROM.h"
+
+#define DEGUB_PRINTING 1
+
+/*
+  ARDUINO PRO MINI 5V/16MHZ - ATMEGA328P - PINOUT
+  -----------------------------------------------
+ | PIN |  PURPOSE                                |
+ |-----|-----------------------------------------|
+ | 9   |  CE   of NRF24L01+                      |
+ | 10  |  CSN  for NRF24L01+                     |
+ | 11  |  MOSI for NRF24L01+                     |
+ | 12  |  MISO for NRF24L01+                     |
+ | 13  |  SCK  for NRF24L01+                     |
+ | A4  |  SDA FOR I2C COMMUNICATION              |
+ | A5  |  SCL FOR I2C COMMUNICATION              |
+ | A6  |  ADC INPUT FOR LM35 Vout                |
+ | A7  |  NC                                     |
+  -----------------------------------------------
+  
+
+  ---------------------------------------------
+ | Power Supply  | Peripheral                  |
+ |---------------|-----------------------------
+ | +5V           |  INA219                     |
+ | +3v3          |  TSL2561                    |
+ | +3v3          |  NRF24L01+                  |
+  ---------------------------------------------
+*/
+
+volatile bool timerFlag = false;
+//---------------------------------------------------------------
+//                  RF24 COMMUNICATION DEFINITIONS
+//---------------------------------------------------------------
+bool     prototypeStatus = false;                                //Control Prototype Online Status
+uint8_t  prototypeStatusCounter = 0;                             //Seconds Counter without a answer from the other prototype
+//Start NRF24L01+ with CE and CS on pins 9 and 10
+RF24 radio(9,10);
+
+// Radio pipe addresses for the nodes to communicate.
+/*
+ * MASTER UNIT PIPE  0xABCDA00
+ * SLAVE 01 PIPE     0xABCDA01
+ */
+const uint64_t pipes[2] = { 0xABCDA00 , 0xABCDA01 }; 
+String messageRF24 = "";
+//---------------------------------------------------------------
+//                        LUXIMETER DEFINITION
+//---------------------------------------------------------------
+SFE_TSL2561 light;                                                //TSL2561 OBJECT
+unsigned int ms = 400;                                            //INTEGRATION ("shutter") TIME IN MILLISECONDS
+double lux;                                                       //LUX OBTAINED
+//---------------------------------------------------------------
+//                      POWER MONITOR DEFINITION
+//---------------------------------------------------------------
+Adafruit_INA219 ina219;
+double energyGenerated = 0;                                      //mWh from This Unit
+double currentGenerated = 0;                                     //mAh from This Unit
+float current_mA  = 0;
+float loadvoltage = 0;
+float power_mW    = 0;
+//---------------------------------------------------------------
+//                         TEMPERATURE LM35
+//---------------------------------------------------------------
+double tempLM35 = 0;
+
+//---------------------------------------------------------------
+//             FUNCTIONS: ARDUINO CONFIGURATION
+//---------------------------------------------------------------
+void configThisUnit()
+{
+  //INPUT | OUTPUT CONFIGURATION
+  Serial.begin(115200);
+
+  if( DEBUG_PRINTING ){ Serial.println("Starting LUX"); }
+  //LUXIMETER START SETUP
+  light.begin();
+  light.setTiming( 0 , 2 , ms );
+  light.setPowerUp();
+
+  if( DEBUG_PRINTING ){ Serial.println("Starting INA219"); }
+  //POWER MONITOR START SETUP
+  ina219.begin();
+  ina219.setCalibration_16V_400mA();                                            // max precision on volts and amps
+
+  if( DEBUG_PRINTING ){ Serial.println("Starting RF24"); }
+  //--------------------------------------------
+  //             RF24 CONFIGURATION
+  //--------------------------------------------
+  //Start Communication
+  radio.begin();
+
+  radio.setPALevel( RF24_PA_LOW   );
+  radio.setDataRate( RF24_250KBPS );
+
+  radio.openWritingPipe(     pipes[0] ); //When Answering the Master
+  //radio.openReadingPipe( 1 , pipes[1] ); //The Pipe where Master Answers This Slave
+  radio.startListening();                // Start listening
+  //--------------------------------------------
+
+  //--------------------------------------------
+  //             TIMER CONFIGURATION
+  //--------------------------------------------
+  noInterrupts();
+  TCCR1A = B00000000;//Register A all 0's since we're not toggling any pins
+    // TCCR1B clock prescalers
+    // 1 x x x CTC mode
+    // x 0 0 1 clkI/O /1 (No prescaling)
+    // x 0 1 0 clkI/O /8 (From prescaler)
+    // x 0 1 1 clkI/O /64 (From prescaler)
+    // x 1 0 0 clkI/O /256 (From prescaler)
+    // x 1 0 1 clkI/O /1024 (From prescaler)
+  TCCR1B = B00001101;//bit 3 set for CTC mode, bits 2,1,0 set to 1024 prescaler
+  TIMSK1 = B00000010;//bit 1 set to call the interrupt on an OCR1A match
+  OCR1A  = (unsigned long)(15625UL); //1 Second = 15625 cicles = 16M / 1024
+  interrupts();
+  //--------------------------------------------
+}
+
+//---------------------------------------------------------------
+
+
+ISR(TIMER1_COMPA_vect){
+  static uint8_t ISRCounter = 0;
+  if( !timerFlag ){
+    ISRCounter++;
+    if( ISRCounter == 60 ){
+      ISRCounter = 0;
+      timerFlag = true; 
+    }
+  }
+}
+
+uint8_t checkTimerFlag()
+{
+  if( timerFlag )
+  {
+    timerFlag = false;
+    return 1;
+  }
+  return 0;
+}
+
+//---------------------------------------------------------------
+//               FUNCTIONS: RF24 COMMUNICATION
+//---------------------------------------------------------------
+void report2Master(){
+  /*
+  char message[10] = "#VxDDDDDD#";
+  message[0] = START_CHAR;
+  
+  message[9] = END_CHAR;
+  */
+  String msg      = ((char)(START_CHAR)) + String("V0") + String( energyGenerated , 2 ) + ((char)(END_CHAR)) + 
+                    ((char)(START_CHAR)) + String("V1") + String(        tempLM35 , 2 ) + ((char)(END_CHAR));
+  
+  char message[60];
+  messageS.toCharArray(msg1 , 60);
+  radio.stopListening();
+  if( DEGUB_PRINTING ){ Serial.println("  Writing Message"); }
+  radio.write( message, sizeof(message) );
+  radio.startListening();
+  if( DEGUB_PRINTING ){ 
+    Serial.println(" ");
+    Serial.print("Text Sent: [");
+    Serial.print( message );
+    Serial.println("]");
+  }
+
+  msg             = ((char)(START_CHAR)) + String("V2") + String(             lux , 2 ) + ((char)(END_CHAR)) + 
+                    ((char)(START_CHAR)) + String("V3") + String(      current_mA , 2 ) + ((char)(END_CHAR));
+  char message[60];
+  messageS.toCharArray(msg1 , 60);
+  radio.stopListening();
+  if( DEGUB_PRINTING ){ Serial.println("  Writing Message"); }
+  radio.write( message, sizeof(message) );
+  radio.startListening();
+  if( DEGUB_PRINTING ){ 
+    Serial.println(" ");
+    Serial.print("Text Sent: [");
+    Serial.print( message );
+    Serial.println("]");
+  }
+
+  msg             = ((char)(START_CHAR)) + String("V4") + String(     loadvoltage , 2 ) + ((char)(END_CHAR)) + 
+                    ((char)(START_CHAR)) + String("V5") + String(currentGenerated , 2 ) + ((char)(END_CHAR));
+  char message[60];
+  messageS.toCharArray(msg1 , 60);
+  radio.stopListening();
+  if( DEGUB_PRINTING ){ Serial.println("  Writing Message"); }
+  radio.write( message, sizeof(message) );
+  radio.startListening();
+  if( DEGUB_PRINTING ){ 
+    Serial.println(" ");
+    Serial.print("Text Sent: [");
+    Serial.print( message );
+    Serial.println("]");
+  }
+
+}
+//---------------------------------------------------------------
+
+
+//---------------------------------------------------------------
+//                      SOLAR TRACKER FUNCTIONS
+//---------------------------------------------------------------
+void refreshSensorData()
+{
+  /*
+    * Get new sensors data:
+    * Power Monitor
+    * Temperature
+    * LUX
+    * Angle (MPU)
+    * Photo and Stepper data are updated somewhere else
+  */
+  
+  //POWER MONITOR
+  current_mA       = ina219.getCurrent_mA();
+  loadvoltage      = ina219.getBusVoltage_V() + (ina219.getShuntVoltage_mV() / 1000);
+  power_mW         = ina219.getPower_mW();
+  energyGenerated  += power_mW    * 1.66666e-2; //mW times (1/60 hours)
+  currentGenerated += current_mA  * 1.66666e-2; //mA times (1/60 hours)
+
+  //TEMPERATURE
+  tempLM35 = (double(analogRead(LM35_PIN))*5/(1023))/0.01;
+
+  //LUX
+  unsigned int data0, data1;
+  if (light.getData(data0,data1))
+  {
+    light.getLux(0,ms,data0,data1,lux);
+  }
+
+  if( DEGUB_PRINTING ){ 
+    Serial.println("Energy Generated = " + String(energyGenerated ,2) + " mWh");
+    Serial.println("Power            = " + String(power_mW        ,2) + " mW");
+    Serial.println("loadvoltage      = " + String(loadvoltage     ,2) + " V");
+    Serial.println("current_mA       = " + String(current_mA      ,2) + " mA");
+    Serial.println("tempL35          = " + String(tempLM35        ,2) + " mA");
+    Serial.println("LUX              = " + String(lux             ,2) + " lx");
+  }
+}
---
+++ Stable_070414/Logger/myFunctions.cpp
@@ -0,0 +1,140 @@
+#include "Arduino.h"
+#include "myFunctions.h"
+//#include "myEEPROM.h"
+
+void logThisLine(String message);
+bool checkForInvalidChars(String *filename);
+//---------------------------------------------------------------
+//                  GLOBAL VARIABLES DEFINITIONS
+//---------------------------------------------------------------
+volatile bool timerFlag     = false;                             //PERIODIC TIMER INTERRUPT FLAG
+const char CharactersInvalidForFileName[] = {'#','<','$','+','%','>','!','`','&','*','“','|','{','?','”','=','}','/',':','\\','@'};
+const uint8_t CharactersInvalidForFileNameLEN = 21;
+
+//---------------------------------------------------------------
+//                     MICRO SD MODULE DEFINITION
+//---------------------------------------------------------------
+SdFat sdCard;
+SdFile todayLog;
+
+
+//---------------------------------------------------------------
+//             FUNCTIONS: ARDUINO CONFIGURATION
+//---------------------------------------------------------------
+void configThisUnit()
+{  
+  
+  //--------------------------------------------
+  //       UART BAUD RATE CONFIGURATION
+  //--------------------------------------------
+  Serial.begin(115200);                             //RECEIVE DATA FROM UART
+  //---------------------------------------------
+
+  String filename = "12345678.123";                 //8.3 format = 8 chars for name + '.' + 3 chars for format extension.
+
+  //Request for data
+  Serial.println("LOG DATE?");                    //REQUEST FILENAME
+  while( 1 )
+  {
+    while( !(Serial.available() > 0 ) ){}         //WAITING FOR MSG
+
+    filename = Serial.readString();
+    if( (filename.indexOf( START_CHAR ) >= 0) &&
+        (filename.indexOf( END_CHAR ) > filename.indexOf( START_CHAR )) &&
+        (((filename.indexOf( END_CHAR )) - filename.indexOf( START_CHAR )) <= 9) )
+    {
+      filename = filename.substring( filename.indexOf( START_CHAR ) + 1 , filename.indexOf( END_CHAR ) ) + ".txt";
+      if( checkForInvalidChars( &filename ) ){ break; }
+    }
+    Serial.println("LOG DATE!");                   //REQUEST A VALID FILENAME
+  }
+  
+
+  //--------------------------------------------
+  //           CREATING FILE
+  //--------------------------------------------
+  //CREATE SD FILE MANAGER
+  if( !sdCard.begin(8,SPI_HALF_SPEED) )
+  {
+    sdCard.initErrorHalt();
+  }
+  if( !todayLog.open( String(filename).c_str()  , O_RDWR | O_CREAT | O_AT_END) )
+  {
+    Serial.println("LOG ERR1");    
+  }
+  //--------------------------------------------
+
+  Serial.println("LOG OK");
+  delay( 500 );
+}
+
+void handleData()
+{
+  String data = "";
+  if( Serial.available() > 0 )
+  {
+    data = Serial.readString();
+    Serial.println("GOT["+data+"]");
+    if( data.indexOf( END_CHAR ) >= 0 )
+    {
+      if( data.indexOf( END_CHAR ) == 0 )
+      {
+        todayLog.close();
+      }
+      else
+      {
+        data = data.substring(0,END_CHAR);
+        logThisLine( data );
+        todayLog.close();
+      }
+      Serial.println("LOG END");
+    }
+    else
+    {
+      logThisLine( data );
+    }
+  }
+}
+
+void logThisLine(String message)
+{
+  while( ( message.length() > 0 ) )
+  {
+    
+    if( (message.indexOf( 13 ) == 0 ) or (message.indexOf( 10 ) == 0 ) )
+    {
+      message = message.substring( 1 );
+    }
+    else if(  message.indexOf( 13 ) > 0 )
+    {
+      String tempData = message.substring(0, message.indexOf( 13 ) );
+      todayLog.println( tempData );
+      Serial.println("["+tempData+"]");
+      message = message.substring( message.indexOf( 13 ) ); 
+    }
+    else if(  message.indexOf( 10 ) > 0 )
+    {
+      String tempData = message.substring(0, message.indexOf( 10 ) );
+      todayLog.println( tempData );
+      message = message.substring( message.indexOf( 10 ) ); 
+    }
+    else
+    {
+      todayLog.print( message );
+      Serial.println("["+message+"]");
+      break;
+    }
+    delay( 50 );
+  }
+}
+
+bool checkForInvalidChars(String *filename)
+{
+  bool valid = true;
+  for( uint8_t i=0; i<CharactersInvalidForFileNameLEN; i++ )
+  {
+    if( (*filename).indexOf( CharactersInvalidForFileName[i] ) > -1 ){ valid = false; break; }
+  }
+  if( valid ){ return 1; }
+  return 0;
+}
